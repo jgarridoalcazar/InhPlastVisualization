1aa2916debbeb2546d2fee26adfa29a972ebe286
jgarridoalcazar <jgarrido@vpn-s241197.ugr.es>
2016-12-04 13:54:52 +0100

    Number of processes in evolutionary algorithms changed
    Evolutionary algorithm creates as many processes and cores use.

--- launch/GoCGrC/LaunchEAClusterPlastNoMFGrC4Hit.sh
+++ launch/GoCGrC/LaunchEAClusterPlastNoMFGrC4Hit.sh
@@ -7,7 +7,7 @@
 #$ -j y
 #$ -R y
 #$ -cwd
-#$ -v OMP_NUM_THREADS=2
+#$ -v OMP_NUM_THREADS=1
 #$ -q 72H
 #$ -pe openmpi 128
 #$ -hold_jid deap4-noMFGrC-learning
@@ -16,4 +16,4 @@ export PATH=/SCRATCH/TIC117/jesusgarrido/autotools/bin:/SCRATCH/TIC117/jesusgarr
 export PYTHONPATH=/SCRATCH/TIC117/jesusgarrido/NEST/nest210/insNoMPI/lib/python2.7/site-packages/:/SCRATCH/TIC117/jesusgarrido/scipy/ins/lib/python2.7/site-packages/:$PYTHONPATH
 export LD_LIBRARY_PATH=/SCRATCH/TIC117/jesusgarrido/NEST/nest210/insNoMPI/lib/nest/:$LD_LIBRARY_PATH
 
-mpirun-test -np $(($NSLOTS / 2 + 1)) -ppn 8 python ./src/LaunchEvolutionaryAlgorithmMPI.py ./config/GoCGrC/EASearchGranularPlastNoMFGrC4ClusterHit.cfg
+mpirun-test -np $(($NSLOTS + 1)) -ppn 8 python ./src/LaunchEvolutionaryAlgorithmMPI.py ./config/GoCGrC/EASearchGranularPlastNoMFGrC4ClusterHit.cfg
