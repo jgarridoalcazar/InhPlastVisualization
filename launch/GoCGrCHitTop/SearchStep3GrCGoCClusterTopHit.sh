1741fb547e04564ef12015ac9a9b02966146dce3
jgarridoalcazar <jgarrido@jgarrido.local>
2017-03-05 10:49:46 +0100

    Bug fixed in Search Step 3
    - Launch file changed.

--- launch/GoCGrCHitTop/SearchStep3GrCGoCClusterTopHit.sh
+++ launch/GoCGrCHitTop/SearchStep3GrCGoCClusterTopHit.sh
@@ -16,4 +16,4 @@ export PATH=/SCRATCH/TIC117/jesusgarrido/autotools/bin:/SCRATCH/TIC117/jesusgarr
 export PYTHONPATH=/SCRATCH/TIC117/jesusgarrido/NEST/nest210/insNoMPI/lib/python2.7/site-packages/:/SCRATCH/TIC117/jesusgarrido/scipy/ins/lib/python2.7/site-packages/:$PYTHONPATH
 export LD_LIBRARY_PATH=/SCRATCH/TIC117/jesusgarrido/NEST/nest210/insNoMPI/lib/nest/:$LD_LIBRARY_PATH
 
-mpirun -np $NSLOTS python ./src/LaunchSearchMPI.py ./config/GoCGrCHitTop/SearchStep3GrCGoClusterTopHit.cfg
+mpirun -np $NSLOTS python ./src/LaunchSearchMPI.py ./config/GoCGrCHitTop/SearchStep3GrCGoCClusterTopHit.cfg
