bae03d85c9d718ee0d55ac1677d18bc378fdf1eb
Jesus Garrido <jgarridoalcazar@gmail.com>
2018-05-21 21:02:17 +0200

    Config files for EA MFGrC FanIn 2000

---
+++ launch/GrCFanIn/EAMFGrCFanIn2000.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#$ -S /bin/sh
+#$ -N MFGrCFanIn2000
+#$ -m bea
+#$ -M jesusgarrido@ugr.es
+#$ -o /SCRATCH/TIC117/jesusgarrido/SpikingGranularLayer/results
+#$ -j y
+#$ -R y
+#$ -cwd
+#$ -v OMP_NUM_THREADS=1
+#$ -q 72H
+#$ -pe openmpi 128
+#$ -hold_jid MFGrCFanIn2000
+
+source $HOME/install/nest/ins/nompi/bin/nest_vars.sh
+
+module load alhambra/gcc-7.3.0
+
+export PATH=$HOME/.local/bin:$HOME/install/cmake/ins/bin:$HOME/install/openssl/ins/bin:$HOME/install/cpython/ins/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/install/gsl/ins/lib:$HOME/install/openssl/ins/lib:$HOME/install/cpython/ins/lib:$HOME/install/readline/ins/lib:$HOME/install/libtool/ins/lib:$NEST_INSTALL_DIR/lib64:$LD_LIBRARY_PATH
+
+
+mpirun -np $(($NSLOTS+1)) python ./src/LaunchEvolutionaryAlgorithmMPI.py ./config/GrCGoCPlasticity/EAMFGrCFanIn2000.cfg
